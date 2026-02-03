f1fee4b4b8e0ee68766cd676992d96b21205a32f
João Dalla Rosa <joao.custodiodllrosa@gmail.com>
2026-02-02 20:32:01 -0600

    create federated policy

---
+++ github-actions/data.tf
@@ -0,0 +1,32 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "repo:OWNER/REPO:ref:refs/heads/main",
+        "repo:OWNER/REPO:ref:refs/heads/release/*"
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+  }
+}
